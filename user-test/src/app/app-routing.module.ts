5de7745009b3fd4637faacefe1b3ee5e723d7595
Sarah Crosby <SarahCroMich@gmail.com>
2020-11-12 11:59:24 -0500

    added edits

--- user-test/src/app/app-routing.module.ts
+++ user-test/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserListComponent } from "./user/user-list/user-list.component";
 
 
 const routes: Routes = [
   {path: "", redirectTo: "/users/list", pathMatch: "full"},
   {path: "users/list", component: UserListComponent },
+  {path: "users/edit/:id", component: UserEditComponent },    //:id means id is a variable
   {path: "**", component: AppComponent }
 ];
 
